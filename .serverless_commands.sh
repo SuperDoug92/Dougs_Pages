0fa71f90c813de1554417283063b73bc9ff6b5b7
Douglas <clarke.doug248@gmail.com>
2021-10-28 21:45:38 -0400

    finished and tested slsh and made updates to templates

--- .serverless_commands.sh
+++ .serverless_commands.sh
@@ -10,47 +10,57 @@ slsh(){
             parent=${parent##*/}
             functionPath=""
             testPath=""
+            curFolder=${PWD##*/} 
             if [ $parent == "services" ]
             then 
-                functionPath = "src/functions"
-                testPath = "src/tests/"
-            elif [ pwd == "services"]
+                functionPath="src/functions"
+                testPath="src/tests/"
+            
+            elif [ $curFolder == "services" ]
             then
-                PS3='Select the service you would like to add this function to:'
-                select serviceName in $(find -maxdepth 1 -type d)                 
-                functionPath = "$serviceName/src/functions"
-                testPath = "$serviceName/src/tests/"
+                echo "This command must be used in a service directory"
+                # PS3='Select the service you would like to add this function to:'
+                # select serviceName in $(find -maxdepth 1 -mindepth 1 -type d | sed 's%^./%%')   
+                # do 
+                #     functionPath="$serviceName/src/functions"
+                #     testPath="$serviceName/src/tests/"
+                #     break
+                # done
             fi
             if [ $functionPath != "" ]
             then
+                # cd $serviceName && serverless plugin install --name serverless-mocha
+                # cd ..
                 sls create function --function $3 --handler "$functionPath/$3.$3" --path $testPath
                 echo "Created $3 function in $functionPath"
                 echo "Created $3 function test in $testPath"
             else
                 echo "This command must be used in a service directory or the services directory"
             fi        
-        fi
-        if [ $2 == 'service' ]
-        then
+        elif [ $2 == 'service' ]
+        then 
             servicePath=""
-            if [ pwd == "backend" ]
+            curFolder=${PWD##*/} 
+            if [ $curFolder == "backend" ]
             then
-                servicePath = "services/$3"
-                serviceTemplatePath = "servicetemplate"
-            elif [ pwd == "services" ]
+                servicePath="services/$3"
+                serviceTemplatePath="servicetemplate"
+            elif [ $curFolder == "services" ]
             then
-                servicePath = "$3"
-                serviceTemplatePath = "../servicetemplate"
+                servicePath=$3
+                serviceTemplatePath="../servicetemplate"
             fi
-            if [ servicePath != ""]
+            if [ "$servicePath" != "" ]
             then
                 sls create --template-path $serviceTemplatePath --path $servicePath
-                rm -r $servicePath/templates/
+                rm -r "$servicePath/templates/"
+                npm install
                 echo "Created $3 service in $servicePath" 
+            else
+                echo "This command must be used in the backend directory or services directory"
             fi
         fi
-    fi
-    if [ $1 == "test" ]
+    elif [ $1 == "test" ]
         then npx mocha src/tests
     fi
 }
